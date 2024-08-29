ef6f7688f976ff83d94b638acc111c8f0b0a8880
Mikael Johansson <mik.json@gmail.com>
2024-08-29 13:59:58 +0200

    Fix so that we update README with badges on the fly

--- go.mod
+++ go.mod
@@ -2,4 +2,9 @@ module github.com/mikejoh/go-credly
 
 go 1.22.4
 
-require golang.org/x/net v0.26.0
+require golang.org/x/net v0.28.0
+
+require (
+	github.com/google/go-github/v64 v64.0.0
+	github.com/google/go-querystring v1.1.0 // indirect
+)
--- main.go
+++ main.go
@@ -12,13 +12,16 @@ import (
 	"strings"
 	"time"
 
+	"github.com/google/go-github/v64/github"
 	"golang.org/x/net/html"
 )
 
 const BaseURL = "https://www.credly.com/"
 
 var (
-	username string
+	username      string
+	ghToken       string
+	commitMessage string
 )
 
 type Badge struct {
@@ -28,6 +31,8 @@ type Badge struct {
 
 func main() {
 	flag.StringVar(&username, "username", "", "Credly username")
+	flag.StringVar(&ghToken, "gh-token", "", "GitHub token")
+	flag.StringVar(&commitMessage, "commit-message", "Update Credly badges!", "Commit message")
 	flag.Parse()
 
 	if username == "" {
@@ -37,11 +42,50 @@ func main() {
 		}
 	}
 
+	if ghToken == "" {
+		ghToken = os.Getenv("GITHUB_TOKEN")
+		if ghToken == "" {
+			log.Fatal("GitHub token is not provided. Please provide it as a command-line argument or set the GITHUB_TOKEN environment variable.")
+		}
+	}
+	ghUser := os.Getenv("GITHUB_USER")
+	if ghUser == "" {
+		log.Fatal("GITHUB_USER environment variable is not set.")
+	}
+
+	ghClient := github.NewClient(nil).WithAuthToken(ghToken)
+
+	ctx := context.Background()
+
+	// TODO: This only works on personal GitHub spaces. Fix so that we can get the README from any repository
+	content, _, _, err := ghClient.Repositories.GetContents(ctx, ghUser, ghUser, "README.md", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	readme, err := content.GetContent()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Find the badges section in the README.md
+	startIndex := strings.Index(readme, "<!--START_BADGES:badges-->")
+	if startIndex == -1 {
+		log.Fatal("START_BADGES section not found in README.md")
+	}
+
+	endIndex := strings.Index(readme, "<!--END_BADGES:badges-->")
+	if endIndex == -1 {
+		log.Fatal("END_BADGES section not found in README.md")
+	}
+
+	// Adjust endIndex to point to the end of the <!--END_BADGES:badges--> tag
+	endIndex += len("<!--END_BADGES:badges-->")
+
 	urlString := BaseURL + "users/" + username + "/badges"
 	parsedURL, err := url.Parse(urlString)
 	if err != nil {
-		fmt.Println("Error:", err)
-		return
+		log.Fatal(err)
 	}
 
 	req, err := http.NewRequest(http.MethodGet, parsedURL.String(), http.NoBody)
@@ -49,7 +93,7 @@ func main() {
 		log.Fatal(err)
 	}
 
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	ctx, cancel := context.WithTimeout(ctx, 5*time.Second)
 	defer cancel()
 
 	req = req.WithContext(ctx)
@@ -80,7 +124,6 @@ func main() {
 					var badge Badge
 					for c := n.FirstChild; c != nil; c = c.NextSibling {
 						if c.Type == html.ElementNode && c.Data == "img" {
-							fmt.Printf("img tag: %+v\n", c) // print the img tag
 							for _, a := range c.Attr {
 								if a.Key == "src" {
 									badge.ImageSrc = a.Val
@@ -102,5 +145,28 @@ func main() {
 	}
 	f(doc)
 
-  fmt.Println(badges)
+	var badgeMarkdown strings.Builder
+	for _, badge := range badges {
+		badgeMarkdown.WriteString(fmt.Sprintf("<img src=\"%s\" alt=\"%s\" />\n", badge.ImageSrc, badge.Alt))
+	}
+
+	readme = readme[:startIndex] + "<!--START_BADGES:badges-->\n" + badgeMarkdown.String() + "<!--END_BADGES:badges-->" + readme[endIndex:]
+
+	_, _, err = ghClient.Repositories.UpdateFile(ctx, ghUser, ghUser, "README.md", &github.RepositoryContentFileOptions{
+		Branch:  github.String("main"),
+		Message: github.String(commitMessage),
+		Committer: &github.CommitAuthor{
+			Name:  github.String("github-actions[bot]"),
+			Email: github.String("41898282+github-actions[bot]@users.noreply.github.com"),
+		},
+		Author: &github.CommitAuthor{
+			Name:  github.String("github-actions[bot]"),
+			Email: github.String("41898282+github-actions[bot]@users.noreply.github.com"),
+		},
+		Content: []byte(readme),
+		SHA:     content.SHA,
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
 }
